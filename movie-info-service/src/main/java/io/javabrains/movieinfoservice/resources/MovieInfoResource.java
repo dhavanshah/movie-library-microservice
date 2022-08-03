b6b29703e2c5709230e525943d5ece80420cba30
dhavanshah <74516243+dhavanshah@users.noreply.github.com>
2022-08-03 02:43:28 +0100

    Add files via upload

---
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/resources/MovieInfoResource.java
@@ -0,0 +1,19 @@
+package io.javabrains.movieinfoservice.resources;
+
+import io.javabrains.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieInfoResource {
+
+
+    @RequestMapping("/{movieId}")
+    public Movie getmovieInfo(@PathVariable("movieId") String movieId) {
+
+        return new Movie(movieId, "Seven");
+
+    }
+}
